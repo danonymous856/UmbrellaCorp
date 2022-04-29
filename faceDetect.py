a51a81ccbb6566505f0f728b09599dd1a4967ffe
danonymous856 <donaldlaishram861@gmail.com>
2022-04-29 13:19:30 +0530

    facedetect.py initial chnages

--- faceDetect.py
+++ faceDetect.py
@@ -5,7 +5,18 @@ trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 img = cv2.imread('RDJ.png')
 # Must convert to gray scale 
 grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# detect faces
+face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+# Draw Rectangle Around Faces
+for (x,y,w,h) in face_coordinates:
+    cv2.rectangle(img,(x,y),(w+x,y+h),(0,255,0),2)
+
+
+print(img)
+
 # Show off
-cv2.imshow('clever Donald',img)
-cv2.waitkey()
+cv2.imshow('clever Donald',grayscaled_img)
+cv2.waitKey()
 print("Code Completed !")
